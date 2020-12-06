ae0680eca7e3906878064eb9e64f5c2d6b954e49
Ethan Lai <ethan.lai21@ycdsbk12.ca>
2020-12-06 00:10:34 +0000

    Green (Successful Test for 2)

--- src/recursionAssignment/Recursion.java
+++ src/recursionAssignment/Recursion.java
@@ -34,9 +34,11 @@ public class Recursion{
 */
 
   public static String changePi(String str) {
-    if (str.length() == 0) {
-      return "";
+    if (str.length() <= 1) {
+      return str;
+    } else if (str.substring(0,2).equals("pi")) {
+      return "3.14" + changePi(str.substring(2));
     }
-    return str.substring(0) + changePi(str.substring(1));
+    return str.substring(0,1) + changePi(str.substring(1));
   }
 }
